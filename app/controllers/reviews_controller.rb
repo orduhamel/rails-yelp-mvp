33f8751093106bfe477ff0a627f931d6b2c7e2c7
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-02-26 16:44:15 +0100

    Restaurant controller and views done + reviews controller and views done except index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,32 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:index, :new, :create]
+
+  def index
+    @reviews = Reviews.all
+  end
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_reviews_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :review, only: [:new, :create]
+    resources :reviews, only: [:index, :new, :create]
   end
 end
